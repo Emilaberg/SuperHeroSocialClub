b574259f5dea34a911d8472b89ebfeb25f503b72
JonhannesNilsson <jonte9203@hotmail.com>
2024-02-06 16:42:58 +0100

    Added things to usermanager, forgot to save

--- SuperheroSocialClub/Models/UserModel.cs
+++ SuperheroSocialClub/Models/UserModel.cs
@@ -2,8 +2,23 @@
 {
     public class UserModel
     {
+        public UserModel(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+
         public int Id { get; set; }
         public string Username { get; set; } = null!;
         public string Password { get; set; } = null!;
+
+        public UserModel()
+        {
+
+        }
     }
+
+
+
 }
