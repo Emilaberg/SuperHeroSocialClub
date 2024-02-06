98c4aa50bf4e76b83a8af6ef1d71a08ac751029d
JonhannesNilsson <jonte9203@hotmail.com>
2024-02-06 16:42:27 +0100

    Added things to usermanager, login etc.

--- SuperheroSocialClub/Managers/UserManager.cs
+++ SuperheroSocialClub/Managers/UserManager.cs
@@ -8,5 +8,19 @@ namespace SuperheroSocialClub.Managers
         {
             new UserModel { Id = 1, Password = "pass", Username = "user" }
         };
+
+        public static bool ValidateUsername(string username)
+        {
+            bool isValidUsername = true;
+
+            foreach (IUser user in _Users)
+            {
+                if (user._Username == username)
+                {
+                    isValidUsername = false;
+                }
+            }
+            return isValidUsername;
+        }
     }
 }
